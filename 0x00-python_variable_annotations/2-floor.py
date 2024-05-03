80a20f6397941d41d9af0b52b35ccf95a0959b24
oumelm <omaymaelmrhasli@gmail.com>
2024-05-03 16:06:37 -0700

    P

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+'''A module that returns the whole number from a float input'''
+
+
+def floor(n: float) -> int:
+    '''a function that reduces a float to an integer'''
+
+    return int(n)
