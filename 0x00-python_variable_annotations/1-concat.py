bebfab55f57f0ae955230711e83ff61aac77c3a6
oumelm <omaymaelmrhasli@gmail.com>
2024-05-03 15:57:04 -0700

    P

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+'''A module that concatenates two strings'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''A function that concatenates 2 strings'''
+
+    return str1 + str2
